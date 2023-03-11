0a5e03c6e58924a9411ac466980852cc4125738c
syahla31 <syahlasyafiqah31@gmail.com>
2023-03-11 19:37:02 +0700

    jomss

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">LARORA</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023 <a href="#">LARORA</a>.</strong> All rights reserved.
 </footer>
