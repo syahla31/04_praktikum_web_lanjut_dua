139256d5d2edab6633813e56bf1a82f6de7c23d5
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-03-15 09:40:42 +0700

    paginate

--- resources/views/matkul.blade.php
+++ resources/views/matkul.blade.php
@@ -12,31 +12,34 @@
             <div class="card-body">
                     <table class="table table-bordered table-striped">
                         <tr>
-                            <th>NO</th>
-                            <th>KODE MK</th>
-                            <th>MATA KULIAH</th>
-                            <th>SKS</th>
-                            <th>JAM</th>
-                            <th>SEMESTER</th>
-                        </tr>
+                                    <th>NO</th>
+                                    <th>KODE MK</th>
+                                    <th>MATA KULIAH</th>
+                                    <th>SKS</th>
+                                    <th>JAM</th>
+                                    <th>SEMESTER</th>
+                                </tr>
 
-                        @foreach($matkuls as $m)
-                        <tr>
-                            <td>{{$m->id}}</td>
-                            <td>{{$m->KODE_MK}}</td>
-                            <td>{{$m->NAMA_MATA_KULIAH}}</td>
-                            <td>{{$m->SKS}}</td>
-                            <td>{{$m->JAM}}</td>
-                            <td>{{$m->SEMESTER}}</td>
-                        </tr>
-                        @endforeach
-                    </table>
+                                @foreach($matkuls as $m)
+                                <tr>
+                                    <td>{{$m->id}}</td>
+                                    <td>{{$m->KODE_MK}}</td>    
+                                    <td>{{$m->NAMA_MATA_KULIAH}}</td>
+                                    <td>{{$m->SKS}}</td>
+                                    <td>{{$m->JAM}}</td>
+                                    <td>{{$m->SEMESTER}}</td>
+                                </tr>
+                                @endforeach
+                            </table>
+                            {!! $matkuls->withQueryString()->links('pagination::bootstrap-5') !!}
+                    
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-    </div>
-    </div>
 </section>
+
+
 
 @endsection
